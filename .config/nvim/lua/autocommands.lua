507be833d6024287695dfc0eb390f6d621bedaf7
David Alfonso <davidag@users.noreply.github.com>
2023-08-03 13:11:30 +0200

    neovim: initial lua config based on lunarvim/nvim-basic-ide
    feats:
    - simplified to avoid using any plugins for now
    - based loosely on my .vimrc (without plugins)
    - all in with keymaps, seem like good defaults

---
+++ .config/nvim/lua/autocommands.lua
@@ -0,0 +1,15 @@
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+    vim.opt_local.number = false
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+})
+
